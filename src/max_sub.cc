4d15b57e0d4d328b712f8764e4b00ccdbbb7783b
Valor <valorzhong@gmail.com>
2015-08-23 01:10:20 +0800

    ci

--- src/max_sub.cc
+++ src/max_sub.cc
@@ -7,10 +7,10 @@ using namespace std;
 
 int max_sub_sum(const vector<int> &a) {
   int max = 0;
-  for (int i = 0; i < a.size(); ++i) {
-    for (int j = i; j < a.size(); ++j) {
+  for (size_t i = 0; i < a.size(); ++i) {
+    for (size_t j = i; j < a.size(); ++j) {
       int currSum = 0;
-      for (int k = i; k < j; ++k) {
+      for (size_t k = i; k < j; ++k) {
         currSum += a[k];
       }
       if (max < currSum) {
@@ -23,9 +23,9 @@ int max_sub_sum(const vector<int> &a) {
 
 int max_sub_sum2(const vector<int> &a) {
   int max = 0;
-  for (int i = 0; i < a.size(); ++i) {
+  for (size_t i = 0; i < a.size(); ++i) {
     int sum = 0;
-    for (int j = i; j < a.size(); ++j) {
+    for (size_t j = i; j < a.size(); ++j) {
       if (sum < sum + a[j]) {
         sum += a[j];
       }
