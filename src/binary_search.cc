bfe2303c376be91481de4db85c4b174db2c3bc4f
Valor <valorzhong@gmail.com>
2015-08-23 14:05:11 +0800

    bug fix

--- src/binary_search.cc
+++ src/binary_search.cc
@@ -10,16 +10,15 @@ using namespace std;
 int binary_search(const vector<int> &a, const int &x) {
   function<int(const int&, const int&)> rec = [&a, &x, &rec] (const int &start, const int &end) {
     int middle = (start + end) / 2;
-    if (middle == start) {
-      return -1;
-    }
     int val = a[middle];
     if (val == x) {
       return middle;
+    } else if (middle == start) {
+      return -1;
     } else if (val > x) {
       return rec(start, middle);
     } else {
-      return rec(middle, end);
+      return rec(middle + 1, end);
     }
   };
   return rec(0, a.size());
@@ -43,7 +42,9 @@ int binary_search2(const vector<int> &a, const int &x) {
 
 TEST_CASE( "binary search", "[binary search]" ) {
   REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 2) == 1);
+  REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 1) == 0);
   REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 5) == 4);
+  REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 6) == 5);
   REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 0) == -1);
   REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 7) == -1);
 }
