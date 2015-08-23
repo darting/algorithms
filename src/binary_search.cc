c40cbf370c79cb0fd05f14c8f4e8043cd61c8c29
Valor <valorzhong@gmail.com>
2015-08-23 13:59:33 +0800

    binary search

---
+++ src/binary_search.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+using namespace std;
+
+int binary_search(const vector<int> &a, const int &x) {
+  function<int(const int&, const int&)> rec = [&a, &x, &rec] (const int &start, const int &end) {
+    int middle = (start + end) / 2;
+    if (middle == start) {
+      return -1;
+    }
+    int val = a[middle];
+    if (val == x) {
+      return middle;
+    } else if (val > x) {
+      return rec(start, middle);
+    } else {
+      return rec(middle, end);
+    }
+  };
+  return rec(0, a.size());
+}
+
+int binary_search2(const vector<int> &a, const int &x) {
+  int start = 0, end = a.size();
+  while (start < end) {
+    int middle = (start + end) / 2;
+    int val = a[middle];
+    if (val == x) {
+      return middle;
+    } else if (val > x) {
+      end = middle;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return -1;
+}
+
+TEST_CASE( "binary search", "[binary search]" ) {
+  REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 2) == 1);
+  REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 5) == 4);
+  REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 0) == -1);
+  REQUIRE(binary_search({ 1, 2, 3, 4, 5, 6 }, 7) == -1);
+}
+
+TEST_CASE( "binary search 2", "[binary search 2]" ) {
+  REQUIRE(binary_search2({ 1, 2, 3, 4, 5, 6 }, 2) == 1);
+  REQUIRE(binary_search2({ 1, 2, 3, 4, 5, 6 }, 1) == 0);
+  REQUIRE(binary_search2({ 1, 2, 3, 4, 5, 6 }, 5) == 4);
+  REQUIRE(binary_search2({ 1, 2, 3, 4, 5, 6 }, 6) == 5);
+  REQUIRE(binary_search2({ 1, 2, 3, 4, 5, 6 }, 0) == -1);
+  REQUIRE(binary_search2({ 1, 2, 3, 4, 5, 6 }, 7) == -1);
+}
